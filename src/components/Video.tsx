d626b9c228287d5f98fed13e52156ea53be1cbce
deepso <deeps.o7o8.y@gmail.com>
2023-03-31 00:07:54 +0530

    add lobby page elems

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,19 +1,29 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
-import { useVideo } from "@huddle01/react/hooks";
+import { useAudio, useVideo } from "@huddle01/react/hooks";
+import { ArrowBigRight, Camera, CameraOff, Mic, MicOff } from "lucide-react";
 
 import { AspectRatio } from "./ui/aspect-ratio";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 
 export default function Video() {
+  const [mic, setMic] = useState(true);
+  const [camera, setCamera] = useState(true);
+
   const { fetchVideoStream, isLoading, error, stopVideoStream, stream } =
     useVideo();
+  const { fetchAudioStream } = useAudio();
 
   const ref = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
-    if (!isLoading && !stream) fetchVideoStream();
+    if (!isLoading && !stream) {
+      fetchVideoStream();
+      fetchAudioStream();
+    }
 
     if (stream && ref.current) {
       ref.current.srcObject = stream;
@@ -23,17 +33,37 @@ export default function Video() {
   if (error) return <div>Error fetching video</div>;
 
   return (
-    <div className="w-96">
-      <AspectRatio
-        ratio={4 / 3}
-        className="flex justify-center items-center animate-opacity-show z-20 h-full w-full rounded-lg border border-orange-600"
-      >
-        {isLoading ? (
-          <div className="animate-pulse bg-orange-300 w-full h-full"></div>
-        ) : (
-          <video ref={ref} autoPlay muted className="-scale-x-100 rounded-lg" />
-        )}
-      </AspectRatio>
+    <div className="flex flex-col justify-center items-center gap-4">
+      <div className="w-96">
+        <AspectRatio
+          ratio={4 / 3}
+          className="flex justify-center items-center animate-opacity-show z-20 h-full w-full rounded-lg border border-orange-600"
+        >
+          {isLoading ? (
+            <div className="animate-pulse bg-orange-300 w-full h-full"></div>
+          ) : (
+            <video
+              ref={ref}
+              autoPlay
+              muted
+              className="-scale-x-100 rounded-lg"
+            />
+          )}
+        </AspectRatio>
+      </div>
+      <div className="flex justify-center items-center gap-4">
+        <Button onClick={() => setCamera((prev) => !prev)}>
+          {camera ? <Camera /> : <CameraOff />}
+        </Button>
+        <Button onClick={() => setMic((prev) => !prev)}>
+          {mic ? <Mic /> : <MicOff />}
+        </Button>
+      </div>
+
+      <div className="flex justify-center items-center gap-4">
+        <Input placeholder="Name" className="w-2/3 border-black" type="text" />
+        <Button className="">&#10132;</Button>
+      </div>
     </div>
   );
 }
