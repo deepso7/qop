bd9425dc6267f1ad32f715f9d442ac8d9cf3904f
deepso <deeps.o7o8.y@gmail.com>
2023-12-10 03:20:35 +0530

    add option to logout

--- apps/qop/src/screens/user.tsx
+++ apps/qop/src/screens/user.tsx
@@ -1,11 +1,24 @@
-import { Button } from "@/components/ui/button";
+import { useMemo } from "react";
+import { useStytch } from "@stytch/react";
+import { User } from "@stytch/vanilla-js";
+import { createAvatar } from "@dicebear/core";
+import { thumbs } from "@dicebear/collection";
+
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import { useStytch } from "@stytch/react";
-import { User } from "@stytch/vanilla-js";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 interface Props {
   user: User;
@@ -14,17 +27,33 @@ interface Props {
 const UserScreen: React.FC<Props> = ({ user }) => {
   const stytch = useStytch();
 
+  const avatar = useMemo(
+    () =>
+      createAvatar(thumbs, {
+        seed: user.emails[0]?.email,
+      }),
+    [user.emails[0]?.email]
+  );
+
   return (
     <div className="p-2 w-full">
       <div className="flex justify-end">
-        <Tooltip>
-          <TooltipTrigger>
-            <Button onClick={() => stytch.session.revoke()}>Logout</Button>
-          </TooltipTrigger>
-          <TooltipContent>
-            <p>logged in as {user.emails[0]?.email}</p>
-          </TooltipContent>
-        </Tooltip>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src={avatar.toDataUriSync()} />
+              <AvatarFallback>{`${user.emails[0]?.email[0]}${user.emails[0]?.email[1]}`}</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>{user.emails[0]?.email}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => stytch.session.revoke()}>
+              Log out
+              <DropdownMenuShortcut>bui bui</DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
